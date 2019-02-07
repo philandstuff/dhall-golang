0913faea6e95f251ac0f387f41dd627c247d6181
Philip Potter <philip.g.potter@gmail.com>
2019-02-07 22:06:28 +0000

    add success parser spec tests
    also, put the spec tests behind a build tag so that they don't run
    automatically because they mostly fail right now.

---
+++ README.md
@@ -0,0 +1,11 @@
+# dhall-golang
+
+Go bindings for dhall.
+
+## Development
+
+### Running the spec tests
+
+The spec tests are disabled by default (because they mostly fail and that would be noisy) but you can run them with:
+
+    go test -tags spec
--- spec_test.go
+++ spec_test.go
@@ -1,8 +1,11 @@
+// +build spec
+
 package main_test
 
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/philandstuff/dhall-golang/parser"
@@ -27,6 +30,31 @@ func TestParserRejects(t *testing.T) {
 
 			if err == nil {
 				t.Errorf("Expected file %s to fail to parse, but it parsed successfully", f.Name())
+			}
+		})
+	}
+}
+
+func TestParserAccepts(t *testing.T) {
+	successesDir := "./dhall-lang/tests/parser/success/"
+	files, err := filepath.Glob(successesDir + "*A.dhall")
+	if err != nil {
+		t.Fatalf("Couldn't read dhall-lang tests: %v\n(Have you pulled submodules?)\n", err)
+	}
+
+	for _, f := range files {
+		reader, openerr := os.Open(f)
+		name := filepath.Base(f)
+		defer reader.Close()
+		if openerr != nil {
+			t.Fatal(openerr)
+		}
+		t.Run(name, func(t *testing.T) {
+
+			_, err := parser.ParseReader(name, reader)
+
+			if err != nil {
+				t.Errorf("Expected file %s to parse successfully, but got error %v", name, err)
 			}
 		})
 	}
