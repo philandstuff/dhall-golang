e8b9bd7ef5b6ebffe84c60b284a2473754edcfbc
Philip Potter <philip.g.potter@gmail.com>
2019-02-09 19:56:51 +0000

    make parser_test cleaner

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,44 +11,44 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func ParseAndCompare(input []byte, expected interface{}) {
-	root, err := parser.Parse("test", input)
+func ParseAndCompare(input string, expected interface{}) {
+	root, err := parser.Parse("test", []byte(input))
 	Expect(err).ToNot(HaveOccurred())
 	Expect(root).To(Equal(expected))
 }
 
-func ParseAndFail(input []byte) {
-	_, err := parser.Parse("test", input)
+func ParseAndFail(input string) {
+	_, err := parser.Parse("test", []byte(input))
 	Expect(err).To(HaveOccurred())
 }
 
 var _ = Describe("Expression", func() {
 	DescribeTable("simple expressions", ParseAndCompare,
-		Entry("Type", []byte(`Type`), Type),
-		Entry("Kind", []byte(`Kind`), Kind),
-		Entry("Sort", []byte(`Sort`), Sort),
-		Entry("Double", []byte(`Double`), Double),
-		Entry("DoubleLit", []byte(`3.0`), DoubleLit(3.0)),
-		Entry("DoubleLit with exponent", []byte(`3E5`), DoubleLit(3e5)),
-		Entry("DoubleLit with sign", []byte(`+3.0`), DoubleLit(3.0)),
-		Entry("DoubleLit with everything", []byte(`-5.0e1`), DoubleLit(-50.0)),
-		Entry("Infinity", []byte(`Infinity`), DoubleLit(math.Inf(1))),
-		Entry("-Infinity", []byte(`-Infinity`), DoubleLit(math.Inf(-1))),
-		Entry("Integer", []byte(`Integer`), Integer),
-		Entry("IntegerLit", []byte(`+1234`), IntegerLit(1234)),
-		Entry("IntegerLit", []byte(`-3`), IntegerLit(-3)),
-		Entry("Identifier", []byte(`x`), Var{"x", 0}),
-		Entry("Identifier with index", []byte(`x@1`), Var{"x", 1}),
+		Entry("Type", `Type`, Type),
+		Entry("Kind", `Kind`, Kind),
+		Entry("Sort", `Sort`, Sort),
+		Entry("Double", `Double`, Double),
+		Entry("DoubleLit", `3.0`, DoubleLit(3.0)),
+		Entry("DoubleLit with exponent", (`3E5`), DoubleLit(3e5)),
+		Entry("DoubleLit with sign", (`+3.0`), DoubleLit(3.0)),
+		Entry("DoubleLit with everything", (`-5.0e1`), DoubleLit(-50.0)),
+		Entry("Infinity", `Infinity`, DoubleLit(math.Inf(1))),
+		Entry("-Infinity", `-Infinity`, DoubleLit(math.Inf(-1))),
+		Entry("Integer", `Integer`, Integer),
+		Entry("IntegerLit", `+1234`, IntegerLit(1234)),
+		Entry("IntegerLit", `-3`, IntegerLit(-3)),
+		Entry("Identifier", `x`, Var{"x", 0}),
+		Entry("Identifier with index", (`x@1`), Var{"x", 1}),
 	)
 	DescribeTable("naturals", ParseAndCompare,
-		Entry("Natural", []byte(`Natural`), Natural),
-		Entry("NaturalLit", []byte(`1234`), NaturalLit(1234)),
-		Entry("NaturalLit", []byte(`3`), NaturalLit(3)),
-		Entry("NaturalPlus", []byte(`3 + 5`), NaturalPlus{NaturalLit(3), NaturalLit(5)}),
+		Entry("Natural", `Natural`, Natural),
+		Entry("NaturalLit", `1234`, NaturalLit(1234)),
+		Entry("NaturalLit", `3`, NaturalLit(3)),
+		Entry("NaturalPlus", `3 + 5`, NaturalPlus{NaturalLit(3), NaturalLit(5)}),
 		// Check that if we skip whitespace, it parses
 		// correctly as function application, not natural
 		// addition
-		Entry("Plus without whitespace", []byte(`3 +5`), &App{NaturalLit(3), IntegerLit(5)}),
+		Entry("Plus without whitespace", `3 +5`, &App{NaturalLit(3), IntegerLit(5)}),
 	)
 	// can't test NaN using ParseAndCompare because NaN ≠ NaN
 	It("handles NaN correctly", func() {
@@ -59,43 +59,43 @@ var _ = Describe("Expression", func() {
 	})
 	DescribeTable("lambda expressions", ParseAndCompare,
 		Entry("simple λ",
-			[]byte(`λ(foo : bar) → baz`),
+			`λ(foo : bar) → baz`,
 			&LambdaExpr{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 		Entry(`simple \`,
-			[]byte(`\(foo : bar) → baz`),
+			`\(foo : bar) → baz`,
 			&LambdaExpr{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 		Entry("with line comment",
-			[]byte("λ(foo : bar) --asdf\n → baz"),
+			"λ(foo : bar) --asdf\n → baz",
 			&LambdaExpr{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 		Entry("with block comment",
-			[]byte("λ(foo : bar) {-asdf\n-} → baz"),
+			"λ(foo : bar) {-asdf\n-} → baz",
 			&LambdaExpr{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 		Entry("simple ∀",
-			[]byte(`∀(foo : bar) → baz`),
+			`∀(foo : bar) → baz`,
 			&Pi{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 		Entry(`simple forall`,
-			[]byte(`forall(foo : bar) → baz`),
+			`forall(foo : bar) → baz`,
 			&Pi{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 		Entry("with line comment",
-			[]byte("∀(foo : bar) --asdf\n → baz"),
+			"∀(foo : bar) --asdf\n → baz",
 			&Pi{
 				"foo", Var{"bar", 0}, Var{"baz", 0}}),
 	)
 	DescribeTable("applications", ParseAndCompare,
 		Entry("identifier application",
-			[]byte(`foo bar`),
+			`foo bar`,
 			&App{
 				Var{"foo", 0},
 				Var{"bar", 0},
 			}),
 		Entry("lambda application",
-			[]byte(`(λ(foo : bar) → baz) quux`),
+			`(λ(foo : bar) → baz) quux`,
 			&App{
 				&LambdaExpr{
 					"foo", Var{"bar", 0}, Var{"baz", 0}},
@@ -105,16 +105,16 @@ var _ = Describe("Expression", func() {
 		// these keywords should fail to parse unless they're part of
 		// a larger expression
 		DescribeTable("keywords", ParseAndFail,
-			Entry("if", []byte(`if`)),
-			Entry("then", []byte(`then`)),
-			Entry("else", []byte(`else`)),
-			Entry("let", []byte(`let`)),
-			Entry("in", []byte(`in`)),
-			Entry("as", []byte(`as`)),
-			Entry("using", []byte(`using`)),
-			Entry("merge", []byte(`merge`)),
-			Entry("constructors", []byte(`constructors`)),
-			Entry("Some", []byte(`Some`)),
+			Entry("if", `if`),
+			Entry("then", `then`),
+			Entry("else", `else`),
+			Entry("let", `let`),
+			Entry("in", `in`),
+			Entry("as", `as`),
+			Entry("using", `using`),
+			Entry("merge", `merge`),
+			Entry("constructors", `constructors`),
+			Entry("Some", `Some`),
 		)
 	})
 })
