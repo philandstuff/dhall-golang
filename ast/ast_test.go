4928f92bb73f2a649a496806a451ff7f5682b7ec
Philip Potter <philip.g.potter@gmail.com>
2019-02-09 13:33:51 +0000

    remove struct labels for readability

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -31,8 +31,8 @@ var _ = DescribeTable("Rule",
 	Entry("Sort → Sort : Type", Sort, Sort, Sort),
 )
 
-func x(i int) Var { return Var{Name: "x", Index: i} }
-func y(i int) Var { return Var{Name: "y", Index: i} }
+func x(i int) Var { return Var{"x", i} }
+func y(i int) Var { return Var{"y", i} }
 
 var _ = DescribeTable("Shift",
 	func(actual Expr, expected Expr) {
@@ -45,20 +45,20 @@ var _ = DescribeTable("Shift",
 	Entry("Shift(5, x@1, x@1) = x@6", Shift(5, x(1), x(1)), x(6)),
 	Entry("Shift(-1, x@1, x@1) = x", Shift(-1, x(1), x(1)), x(0)),
 	Entry("Shift(5, x, λ(x : Natural) -> x)) = no change",
-		Shift(5, x(0), &LambdaExpr{Label: "x", Type: Natural, Body: x(0)}),
-		&LambdaExpr{Label: "x", Type: Natural, Body: x(0)}),
+		Shift(5, x(0), &LambdaExpr{"x", Natural, x(0)}),
+		&LambdaExpr{"x", Natural, x(0)}),
 	Entry("Shift(5, x, λ(x : Natural) -> x@1)) = λ(x : Natural) -> x@6)",
-		Shift(5, x(0), &LambdaExpr{Label: "x", Type: Natural, Body: x(1)}),
-		&LambdaExpr{Label: "x", Type: Natural, Body: x(6)}),
+		Shift(5, x(0), &LambdaExpr{"x", Natural, x(1)}),
+		&LambdaExpr{"x", Natural, x(6)}),
 	Entry("Shift(5, x, ∀(x : Natural) -> x)) = no change",
-		Shift(5, x(0), &Pi{Label: "x", Type: Natural, Body: x(0)}),
-		&Pi{Label: "x", Type: Natural, Body: x(0)}),
+		Shift(5, x(0), &Pi{"x", Natural, x(0)}),
+		&Pi{"x", Natural, x(0)}),
 	Entry("Shift(5, x, ∀(x : Natural) -> x@1)) = λ(x : Natural) -> x@6)",
-		Shift(5, x(0), &Pi{Label: "x", Type: Natural, Body: x(1)}),
-		&Pi{Label: "x", Type: Natural, Body: x(6)}),
+		Shift(5, x(0), &Pi{"x", Natural, x(1)}),
+		&Pi{"x", Natural, x(6)}),
 	Entry("Shift(1, x, Natural) = Natural", Shift(1, x(0), Natural), Natural),
 	Entry("Shift(1, x, 3) = 3", Shift(1, x(0), NaturalLit(3)), NaturalLit(3)),
-	Entry("Shift(1, x, x + 3) = x@1 + 3", Shift(1, x(0), NaturalPlus{L: x(0), R: NaturalLit(3)}), NaturalPlus{L: x(1), R: NaturalLit(3)}),
+	Entry("Shift(1, x, x + 3) = x@1 + 3", Shift(1, x(0), NaturalPlus{x(0), NaturalLit(3)}), NaturalPlus{x(1), NaturalLit(3)}),
 )
 
 var _ = DescribeTable("Subst",
@@ -68,16 +68,16 @@ var _ = DescribeTable("Subst",
 	Entry("Subst(_, _, Type) = Type", Subst(x(0), Sort, Type), Type),
 	Entry("Subst(_, _, Natural) = Natural", Subst(x(0), Sort, Natural), Natural),
 	Entry("Subst(_, _, 3) = Natural", Subst(x(0), Sort, NaturalLit(3)), NaturalLit(3)),
-	Entry("Subst(x, 10, x + 3) = 10 + 3", Subst(x(0), NaturalLit(10), NaturalPlus{L: x(0), R: NaturalLit(3)}), NaturalPlus{L: NaturalLit(10), R: NaturalLit(3)}),
+	Entry("Subst(x, 10, x + 3) = 10 + 3", Subst(x(0), NaturalLit(10), NaturalPlus{x(0), NaturalLit(3)}), NaturalPlus{NaturalLit(10), NaturalLit(3)}),
 	Entry("Subst(x, Natural, x) = Natural", Subst(x(0), Natural, x(0)), Natural),
 	Entry("Subst(x@1, Natural, x) = x", Subst(x(1), Natural, x(0)), x(0)),
 	Entry("Subst(x, Natural, x@1) = x@1", Subst(x(0), Natural, x(1)), x(1)),
-	Entry("Subst(x, Natural, λ(x: x) -> x) = λ(x: Natural) -> x",
-		Subst(x(0), Natural, &LambdaExpr{Label: "x", Type: x(0), Body: x(0)}),
-		&LambdaExpr{Label: "x", Type: Natural, Body: x(0)}),
+	Entry("Subst(x, Natural, λ( x) -> x) = λ( Natural) -> x",
+		Subst(x(0), Natural, &LambdaExpr{"x", x(0), x(0)}),
+		&LambdaExpr{"x", Natural, x(0)}),
 	Entry("Subst(x, Natural, (λ(x: x) -> x) 3) = (λ(x: Natural) -> x) 3",
-		Subst(x(0), Natural, &App{Fn: &LambdaExpr{Label: "x", Type: x(0), Body: x(0)}, Arg: NaturalLit(3)}),
-		&App{Fn: &LambdaExpr{Label: "x", Type: Natural, Body: x(0)}, Arg: NaturalLit(3)}),
+		Subst(x(0), Natural, &App{&LambdaExpr{"x", x(0), x(0)}, NaturalLit(3)}),
+		&App{&LambdaExpr{"x", Natural, x(0)}, NaturalLit(3)}),
 )
 
 var _ = Describe("TypeCheck in empty context", func() {
@@ -90,50 +90,39 @@ var _ = Describe("TypeCheck in empty context", func() {
 		Entry("Type : Kind", Type, Kind),
 		Entry("Kind : Sort", Kind, Sort),
 		Entry("3 : Natural", NaturalLit(3), Natural),
-		Entry("3 + 5 : Natural", NaturalPlus{L: NaturalLit(3), R: NaturalLit(5)}, Natural),
+		Entry("3 + 5 : Natural", NaturalPlus{NaturalLit(3), NaturalLit(5)}, Natural),
 		Entry("λ(x : Natural) → x : ∀(x : Natural) → Natural",
-			&LambdaExpr{Label: "x", Type: Natural, Body: Var{Name: "x"}},
-			&Pi{Label: "x", Type: Natural, Body: Natural}),
+			&LambdaExpr{"x", Natural, x(0)},
+			&Pi{"x", Natural, Natural}),
 		Entry("(λ(x : Natural) → x) 3 : Natural",
-			&App{Fn: &LambdaExpr{Label: "x", Type: Natural, Body: Var{Name: "x"}},
-				Arg: NaturalLit(3)},
+			&App{&LambdaExpr{"x", Natural, x(0)},
+				NaturalLit(3)},
 			Natural),
 		Entry("(λ(x : Type) → λ(x : x) → x) : ∀(x : Type) → ∀(x : x) → x@1",
 			&LambdaExpr{
-				Label: "x",
-				Type:  Type,
-				Body: &LambdaExpr{
-					Label: "x",
-					Type:  x(0),
-					Body:  x(0)}},
-			&Pi{Label: "x", Type: Type,
-				Body: &Pi{Label: "x", Type: x(0),
-					Body: x(1)}}),
+				"x",
+				Type,
+				&LambdaExpr{
+					"x",
+					x(0),
+					x(0)}},
+			&Pi{"x", Type,
+				&Pi{"x", x(0), x(1)}}),
 		Entry("(λ(x : Type) → λ(x : x) → x) Natural : ∀(x : Natural) → Natural",
 			&App{
-				Fn: &LambdaExpr{
-					Label: "x",
-					Type:  Type,
-					Body: &LambdaExpr{
-						Label: "x",
-						Type:  x(0),
-						Body:  x(0)}},
-				Arg: Natural,
+				&LambdaExpr{"x", Type,
+					&LambdaExpr{"x", x(0), x(0)}},
+				Natural,
 			},
-			&Pi{Label: "x", Type: Natural, Body: Natural}),
+			&Pi{"x", Natural, Natural}),
 		Entry("(λ(x : Type) → λ(x : x) → x) Natural 3 : Natural",
 			&App{
-				Fn: &App{
-					Fn: &LambdaExpr{
-						Label: "x",
-						Type:  Type,
-						Body: &LambdaExpr{
-							Label: "x",
-							Type:  Var{Name: "x"},
-							Body:  Var{Name: "x"}}},
-					Arg: Natural,
+				&App{
+					&LambdaExpr{"x", Type,
+						&LambdaExpr{"x", x(0), x(0)}},
+					Natural,
 				},
-				Arg: NaturalLit(3),
+				NaturalLit(3),
 			},
 			Natural),
 	)
@@ -142,7 +131,7 @@ var _ = Describe("TypeCheck in empty context", func() {
 			_, err := in.TypeWith(EmptyContext())
 			Expect(err).To(HaveOccurred())
 		},
-		Entry("3 +5", &App{Fn: NaturalLit(3), Arg: IntegerLit(5)}),
+		Entry("3 +5", &App{NaturalLit(3), IntegerLit(5)}),
 	)
 })
 
