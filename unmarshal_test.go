cbd3deeb61ce6670ac21bc6251beab369726c6c6
Philip Potter <philip.g.potter@gmail.com>
2019-02-23 16:02:06 +0000

    table-driven tests

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -1,42 +1,38 @@
 package main_test
 
 import (
+	"reflect"
+
 	. "github.com/philandstuff/dhall-golang"
 	"github.com/philandstuff/dhall-golang/ast"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
-func UnmarshalAndCompare(input ast.Expr, expected interface{}) {
-	var actual interface{}
-	Unmarshal(input, actual)
-	Expect(actual).To(Equal(expected))
+func UnmarshalAndCompare(input ast.Expr, ptr interface{}, expected interface{}) {
+	Unmarshal(input, ptr)
+	// use reflect to dereference a pointer of unknown type
+	Expect(reflect.ValueOf(ptr).Elem().Interface()).
+		To(Equal(expected))
 }
 
 var _ = Describe("Unmarshal", func() {
-	Describe("Simple types", func() {
-		It("unmarshals NaturalLit into *int", func() {
-			var i int
-			Unmarshal(ast.NaturalLit(5), &i)
-			Expect(i).To(Equal(5))
-		})
-		It("unmarshals IntegerLit into *int", func() {
-			var i int
-			Unmarshal(ast.IntegerLit(5), &i)
-			Expect(i).To(Equal(5))
-		})
-	})
-	Describe("Compound types", func() {
-		It("unmarshals List Integer into slice", func() {
-			var i []int
-			Unmarshal(ast.MakeList(ast.IntegerLit(5)), &i)
-			Expect(i).To(Equal([]int{5}))
-		})
-		It("unmarshals List Bool into slice", func() {
-			var i []bool
-			Unmarshal(ast.MakeList(ast.True, ast.False), &i)
-			Expect(i).To(Equal([]bool{true, false}))
-		})
-	})
+	DescribeTable("Simple types", UnmarshalAndCompare,
+		Entry("unmarshals NaturalLit into *int",
+			ast.NaturalLit(5), new(int), 5),
+		Entry("unmarshals IntegerLit into *int",
+			ast.IntegerLit(5), new(int), 5),
+	)
+	DescribeTable("Compound types", UnmarshalAndCompare,
+		Entry("unmarshals List Integer into slice",
+			ast.MakeList(ast.IntegerLit(5)),
+			new([]int),
+			[]int{5}),
+		Entry("unmarshals List Bool into slice",
+			ast.MakeList(ast.True, ast.False),
+			new([]bool),
+			[]bool{true, false}),
+	)
 })
