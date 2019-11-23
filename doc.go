2b72f8ffa32707e24d680bc1fe86c4abd39458d2
Philip Potter <philip.g.potter@gmail.com>
2019-11-23 17:01:32 +0000

    add some proper docs

---
+++ doc.go
@@ -0,0 +1,30 @@
+/*
+Package dhall implements routines for deserializing and evaluating
+Dhall configuration into Go data structures.
+
+For more on the Dhall language, see https://dhall-lang.org/
+
+If you have the following struct:
+
+ type Message struct {
+	 Name string
+	 Body string
+	 Time int64
+ }
+
+And a file called foo.dhall containing the following Dhall
+configuration:
+
+ { Name = "Alice", Body = "Hello", Time = 1294706395881547000 }
+
+You can deserialize the Dhall into the struct as follows (error
+handling skipped for brevity):
+
+ var m Message
+ dhallBytes, err := ioutil.ReadFile("foo.dhall")
+ err = dhall.Unmarshal(dhallBytes, &m)
+
+This version supports Dhall standard 11.1.0, except that it doesn't
+support `using` directives.
+*/
+package dhall
