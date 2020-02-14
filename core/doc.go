8b09c463c25ae0e03576a55045fb57883fd6c25c
Philip Potter <philip.g.potter@gmail.com>
2020-02-14 14:04:33 +0000

    update core docs

--- core/doc.go
+++ core/doc.go
@@ -1,9 +1,9 @@
 /*
 Package core contains the core Dhall language implementation.
 
-The main types are Term and Value.  A Term represents an arbitrary
-Dhall expression, while a Value represents a fully beta-normalized
-Dhall value.  Eval() takes Terms and normalizes them to Values;
-Quote() takes Values and expresses them as Terms.
+If you have a Term, you can call TypeOf() to ensure it is well-typed
+and to get its type.  A well-typed Term can be Eval()ed to return a
+Value.  A Value represents a fully beta-normalized Dhall value.  If
+you have a Value, you can use Quote() to express it as a Term.
 */
 package core
