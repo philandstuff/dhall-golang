4d7933b65cc528c2123efbe367e431e64681e709
Philip Potter <philip.g.potter@gmail.com>
2020-02-16 04:37:28 +0000

    pull in dhall-lang v14.0.0

--- spec_test.go
+++ spec_test.go
@@ -26,6 +26,19 @@ var slowTests = []string{
 }
 
 var expectedFailures = []string{
+	// v14.0.0 features
+	"TestParserRejects/unit/NaturalLitLeadingZero",
+	"TestTypeInferenceFails/unit/RecordLitDuplicateFields",
+	"TestNormalization/unit/RecordLitNixLike",
+	"TestNormalization/unit/RecordLitDuplicateFields",
+	"TestNormalization/unit/RecordLitDottedFields",
+	"TestNormalization/unit/RecordLitTriplicateFields",
+	"TestTypeInference/unit/RecordLitDuplicateFields",
+	"TestTypeInference/unit/RecordLitDotted",
+	"TestParserAccepts/unit/RecordLitNixLike",
+	"TestParserAccepts/unit/RecordLitDuplicates",
+	"TestParserAccepts/unit/RecordLitDotted",
+
 	// needs `using`
 	"TestParserAccepts/unit/import/Headers",
 	"TestParserAccepts/unit/import/inlineUsing",
