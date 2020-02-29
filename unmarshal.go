b84f54909ff136bf1b1e8fc9b24810a04655ccc1
Philip Potter <philip.g.potter@gmail.com>
2020-02-29 14:12:20 +0000

    [unmarshal] refactor decode()
    This refactors the decode() switch to go through the Kinds in the order
    given in the reflect docs.  It also adds a panic() at the bottom and
    returns early from all the successful cases.
    
    This will allow us to change decode() to return an error instead of
    panicking in future.

--- unmarshal.go
+++ unmarshal.go
@@ -85,7 +85,7 @@ func encode(val reflect.Value, typ core.Value) core.Value {
 			panic("foo")
 		}
 		if !isMapEntryType(mapEntryType) {
-			panic("Can't unmarshal golang map into given Dhall type")
+			panic("Can't encode golang map into given Dhall type")
 		}
 		if val.Len() == 0 {
 			return core.EmptyList{Type: mapEntryType}
@@ -181,77 +181,105 @@ func flattenOptional(e core.Value) core.Value {
 func decode(e core.Value, v reflect.Value) {
 	e = flattenOptional(e)
 	if e == nil {
+		// TODO: should we fail if a None doesn't match the type?
+		// (similar to EmptyList below)
 		return
 	}
 	switch v.Kind() {
+	case reflect.Bool:
+		if e, ok := e.(core.BoolLit); ok {
+			v.SetBool(bool(e))
+			return
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		if e, ok := e.(core.IntegerLit); ok {
+			v.SetInt(int64(e))
+			return
+		}
+		if e, ok := e.(core.NaturalLit); ok {
+			v.SetInt(int64(e))
+			return
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		if e, ok := e.(core.NaturalLit); ok {
+			v.SetUint(uint64(e))
+			return
+		}
+	case reflect.Float32, reflect.Float64:
+		if e, ok := e.(core.DoubleLit); ok {
+			v.SetFloat(float64(e))
+			return
+		}
+	case reflect.Func:
+		if e, ok := e.(core.Callable); ok {
+			fnType := v.Type()
+			returnType := fnType.Out(0)
+			fn := reflect.MakeFunc(fnType, dhallShim(returnType, e))
+			v.Set(fn)
+			return
+		}
 	case reflect.Map:
 		// initialise with new (non-nil) value
 		v.Set(reflect.MakeMap(v.Type()))
 		if _, ok := e.(core.EmptyList); ok {
+			// TODO: should this fail if type mismatches?
+			// it should at least be a mapKey/mapValue type
 			return
 		}
-		e := e.(core.NonEmptyList)
-		recordLit := e[0].(core.RecordLit)
-		if len(recordLit) != 2 {
-			panic("can only unmarshal `List {mapKey : T, mapValue : U}` into go map")
-		}
-		for _, r := range e {
-			entry := r.(core.RecordLit)
-			key := reflect.New(v.Type().Key()).Elem()
-			val := reflect.New(v.Type().Elem()).Elem()
-			decode(entry["mapKey"], key)
-			decode(entry["mapValue"], val)
-			v.SetMapIndex(key, val)
-		}
-	case reflect.Struct:
-		e := e.(core.RecordLit)
-		structType := v.Type()
-		for i := 0; i < structType.NumField(); i++ {
-			// FIXME ignores fields in RecordLit not in Struct
-			tag := structType.Field(i).Tag.Get("dhall")
-			if tag != "" {
-				decode(e[tag], v.Field(i))
-			} else {
-				decode(e[structType.Field(i).Name], v.Field(i))
+		if e, ok := e.(core.NonEmptyList); ok {
+			recordLit := e[0].(core.RecordLit)
+			// TODO: use isMapEntryType() here?
+			if len(recordLit) != 2 {
+				panic("can only unmarshal `List {mapKey : T, mapValue : U}` into go map")
 			}
+			for _, r := range e {
+				entry := r.(core.RecordLit)
+				key := reflect.New(v.Type().Key()).Elem()
+				val := reflect.New(v.Type().Elem()).Elem()
+				decode(entry["mapKey"], key)
+				decode(entry["mapValue"], val)
+				v.SetMapIndex(key, val)
+			}
+			return
 		}
-	case reflect.Func:
-		e := e.(core.Callable)
-		fnType := v.Type()
-		returnType := fnType.Out(0)
-		fn := reflect.MakeFunc(fnType, dhallShim(returnType, e))
-		v.Set(fn)
+	case reflect.Ptr:
+		v.Set(reflect.New(v.Type().Elem()))
+		decode(e, v.Elem())
+		return
 	case reflect.Slice:
 		if _, ok := e.(core.EmptyList); ok {
 			v.Set(reflect.MakeSlice(v.Type(), 0, 0))
 			return
 		}
-		e := e.(core.NonEmptyList)
-		slice := reflect.MakeSlice(v.Type(), len(e), len(e))
-		for i, expr := range e {
-			decode(expr, slice.Index(i))
+		if e, ok := e.(core.NonEmptyList); ok {
+			slice := reflect.MakeSlice(v.Type(), len(e), len(e))
+			for i, expr := range e {
+				decode(expr, slice.Index(i))
+			}
+			v.Set(slice)
+			return
 		}
-		v.Set(slice)
-	case reflect.Ptr:
-		v.Set(reflect.New(v.Type().Elem()))
-		decode(e, v.Elem())
-	case reflect.Uint, reflect.Uint8, reflect.Uint16,
-		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		v.SetUint(uint64(e.(core.NaturalLit)))
-	default:
-		switch e := e.(type) {
-		case core.DoubleLit:
-			v.SetFloat(float64(e))
-		case core.BoolLit:
-			v.SetBool(bool(e))
-		case core.NaturalLit:
-			v.SetInt(int64(e))
-		case core.IntegerLit:
-			v.SetInt(int64(e))
-		case core.PlainTextLit:
+	case reflect.String:
+		if e, ok := e.(core.PlainTextLit); ok {
 			v.SetString(string(e))
-		default:
-			panic(fmt.Sprintf("Don't know how to decode %v into %v", e, v.Kind()))
+			return
+		}
+	case reflect.Struct:
+		if e, ok := e.(core.RecordLit); ok {
+			structType := v.Type()
+			for i := 0; i < structType.NumField(); i++ {
+				// FIXME ignores fields in RecordLit not in Struct
+				tag := structType.Field(i).Tag.Get("dhall")
+				if tag != "" {
+					decode(e[tag], v.Field(i))
+				} else {
+					decode(e[structType.Field(i).Name], v.Field(i))
+				}
+			}
+			return
 		}
 	}
+	panic(fmt.Sprintf("Don't know how to decode %v into %v", e, v.Kind()))
 }
